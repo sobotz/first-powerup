ffb74622b029e95835a8bbd3a939289162a67905
Olivia Steger <InspectorElement@gmail.com>
2018-02-20 10:58:48 -0500

    Update Winch.java

--- src/org/usfirst/frc/team6911/robot/Winch.java
+++ src/org/usfirst/frc/team6911/robot/Winch.java
@@ -2,16 +2,16 @@ package org.usfirst.frc.team6911.robot;
 
 /*
 public class Winch {
-	private static OI winchController;
 	public Winch() {
-		winchController = new OI(Robotmap.winchController);
 		Robotmap.winchMotor.set(0.0);
 	}
 	public void WinchControl() {
-		if (winchController.getLtrigger() & !winchController.getRtrigger()) {
+		if (liftController.getLeftTopButton() & liftController.getRightTopButton()) {
 			Robotmap.winchMotor.set(0.5);
-		} else if (winchController.getRtrigger() & !winchController.getLtrigger()) {
-			Robotmap.winchMotor.set(-0.5);
+		} else if (liftController.getLeftTopButton() & !liftController.getRightTopButton()) {
+			Robotmap.winchMotor.set(0.0);
+		} else if (!liftController.getLeftTopButton() & liftController.getRightTopButton()) {
+			Robotmap.winchMotor.set(0.0);
 		} else {
 			Robotmap.winchMotor.set(0.0);
 		}
